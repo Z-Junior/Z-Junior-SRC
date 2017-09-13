2e11dbf155e88b5ad0a9329f26bdc6c80421a3fd
RumbleFrog <admin@biturl.io>
2017-09-13 17:25:21 -0400

    Bulma Pre-Processor

--- src/App.vue
+++ src/App.vue
@@ -56,13 +56,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/bulma/bulma.sass" lang="sass"></style>
